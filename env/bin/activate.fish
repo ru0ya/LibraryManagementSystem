ced02b9ca25266fefc6379a8d830dd346f9c060d
ru0ya <viktaangi02@gmail.com>
2024-02-26 09:16:59 +0300

    deploying

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ruoya/LibraryManagementWebApp/env"
+set -gx VIRTUAL_ENV "/home/ruoya/Maktaba/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
